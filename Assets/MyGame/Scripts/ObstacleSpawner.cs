09b528dc97753cd7894ee2dac2f023fa359c7923
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-12-15 20:24:24 +0100

    refactoring: ObstacleSpawner deleted unnecassary spaces

--- Assets/MyGame/Scripts/ObstacleSpawner.cs
+++ Assets/MyGame/Scripts/ObstacleSpawner.cs
@@ -22,22 +22,17 @@ public class ObstacleSpawner : MonoBehaviour
     IEnumerator Spawn()
     {
         float waitTime = 1f;
-
         yield return new WaitForSeconds(waitTime);
-
         while (!gameOver)
         {
             SpawnObstacle();
-
             waitTime = Random.Range(minSpawnTime, maxSpawnTime);
-
             yield return new WaitForSeconds(waitTime);
         }
     }
     void SpawnObstacle()
     {
         int random = Random.Range(0, obstacles.Length);
-
         Instantiate(obstacles[random], transform.position, Quaternion.identity);
     }
 }
