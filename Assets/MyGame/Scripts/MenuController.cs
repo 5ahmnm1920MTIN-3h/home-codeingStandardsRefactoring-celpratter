ff8fcdad600348118d6c2bf5238c5228bb234d1b
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-12-16 19:30:37 +0100

    refactoring: MenuController removed magic string, deleted unused namespaces

--- Assets/MyGame/Scripts/MenuController.cs
+++ Assets/MyGame/Scripts/MenuController.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuController : MonoBehaviour{
-    public void Play(){
-        SceneManager.LoadScene("MainScene");
+public class MenuController : MonoBehaviour
+{
+    private string mainScene = "MainScene";
+    public void Play()
+    {
+        SceneManager.LoadScene(mainScene);
     }
-
     public void Exit()
     {
         Application.Quit();
